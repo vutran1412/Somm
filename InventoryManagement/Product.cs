0e60b30fb26beaf1ee7714c6ebc2149fcb1caf67
Vu Tran <vutran1412@icloud.com>
2018-04-01 02:32:20 -0500

    Changed form name

--- InventoryManagement/Product.cs
+++ InventoryManagement/Product.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace InventoryManagement
 {
-    class Product
+    public class Product
     {
+        private string productName;
+        private int productQuantity;
+        private double productPrice;
+        private DateTime productRecieved = DateTime.Now;
+
+        public Product() { }
+
+        public Product(string productName, int productquantity, double productPrice, 
+            DateTime productRecieved)
+        {
+
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+            set
+            {
+                productName = value;
+            }
+        }
+
+        public int ProductQuantity
+        {
+            get
+            {
+                return productQuantity;
+            }
+            set
+            {
+                productQuantity = value;
+            }
+        }
+
+        public double ProductPrice
+        {
+            get
+            {
+                return productPrice;
+            }
+            set
+            {
+                productPrice = value;
+            }
+        }
+
+        public DateTime ProductRecieved
+        {
+            get
+            {
+                return productRecieved;
+            }
+            set
+            {
+                productRecieved = value;
+            }
+        }
+
+        public string GetDisplayText(string sep)
+        {
+            return productName.ToString() + sep + productQuantity.ToString()
+                + sep + productPrice.ToString("c") 
+                + sep + productRecieved.ToString("{0:d}");
+        }
     }
+
+    
 }
